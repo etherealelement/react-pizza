7d67b47ced07e5a16d0c932e456fed83f8934a7f
ethereal element <vitalik.safonov@list.ru>
2023-05-07 20:53:22 +0300

    fix: button props

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -3,6 +3,6 @@ import { ButtonProps } from './Button.props';
 import styles from "./Button.module.scss";
 
 
-export const Button: FC<ButtonProps> = ({variant,text, className, children, ...props}): JSX.Element => {
+export const Button: FC<ButtonProps> = ({variant,text, ...props}): JSX.Element => {
 	return <div>Button</div>;
 };
