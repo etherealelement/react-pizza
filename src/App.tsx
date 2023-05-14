2f9d6a7a70f67f7ebee67070914528a4bf4354c9
ethereal element <vitalik.safonov@list.ru>
2023-05-14 21:59:49 +0300

    byn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.scss"
-
+import { Button } from "./components/ui/button/Button"
 function App() {
 
   return (
     <>
-    
+    <Button variant="primary"></Button>
     </>
   )
 }
