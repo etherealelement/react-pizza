94907e82351320d3cd1e9aa703bf546c4d455632
ethereal element <vitalik.safonov@list.ru>
2023-05-27 10:30:47 +0300

    fix: button state props

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -2,7 +2,7 @@ import { FC, useState } from "react";
 import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.scss";
 import cn from "classnames";
-import { ReactComponent as Plus } from '../../../assets/plus.svg';
+import { ReactComponent as Plus } from "../../../assets/plus.svg";
 
 export const Button: FC<ButtonProps> = ({
 	variant = "actived",
@@ -11,8 +11,7 @@ export const Button: FC<ButtonProps> = ({
 	isPlus,
 	...props
 }): JSX.Element => {
-	const [plus, setPlus] = useState(false);
-
+	const [plus, setPlus] = useState(0);
 
 	return (
 		<>
@@ -20,12 +19,17 @@ export const Button: FC<ButtonProps> = ({
 				className={cn(styles.button, className, {
 					[styles.default]: variant === "default",
 					[styles.actived]: variant === "actived",
-					[styles.primary]: plus === true,
 				})}
 				{...props}
-				onClick={() => setPlus(true)}
+				onClick={() => setPlus(plus + 1)}
 			>
-			{plus ? <span className={styles.span}>Добавлено !</span> : <><Plus className={styles.plus}></Plus><span className={styles.span}>{children}</span></>}
+				<>
+					<Plus className={styles.plus}></Plus>
+					<span className={styles.span}>
+						{children}{" "}
+						{plus === 0 ? null : <span className={styles.cartCounter}>{plus}</span>}
+					</span>
+				</>
 			</button>
 		</>
 	);
