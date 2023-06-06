ffb1633a69089a51ea1e416dbf7a676b2811e3ae
ethereal element <vitalik.safonov@list.ru>
2023-06-06 16:06:30 +0300

    fix: fullpizza types

--- src/pages/fullpizza/FullPizza.tsx
+++ src/pages/fullpizza/FullPizza.tsx
@@ -1,53 +1,54 @@
-import { FC, useEffect, useState } from "react";
+import {FC, useEffect, useState} from "react";
 import styles from "./FullPizza.module.scss";
-import { useParams, useNavigate } from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import axios from "axios";
+import {FullPizzaProps} from "./FullPizza.props.ts";
+
 
 export const FullPizza: FC = (): JSX.Element => {
-	const { id } = useParams();
-	const [data, setData] = useState();
-	const navigate = useNavigate();
+    const {id} = useParams();
+    const [data, setData] = useState<FullPizzaProps>();
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        const loadData = async (): Promise<void> => {
+            try {
+                const {data} = await axios.get(
+                    "https://64732640d784bccb4a3c4d81.mockapi.io/items/" + id
+                );
+                setData(data);
+            } catch (error) {
+                navigate("/")
+                alert(error);
+            }
+        };
+        loadData();
+    }, []);
+    console.log(data);
 
-	useEffect(() => {
-		const loadData = async ():Promise<void>=> {
-			try {
-				const { data } = await axios.get(
-					"https://64732640d784bccb4a3c4d81.mockapi.io/items/" + id
-				);
-				setData(data);
-			} catch (error) {
-				navigate("/")
-				alert(error);
-			}
-		};
-		loadData();
-	}, []);
-	console.log(data);
+    if (!data) {
+        return  <>"Загрузка..."</>;
+    }
 
-	if (!data) {
-		<>return "Загрузка..."</>;
-	}
-
-	return (
-		<div className={styles.container}>
-			<div className={styles.wrap}>
-				<h2 className={styles.wrapTitle}>{data.title}</h2>
-				<img
-					src={data.imageUrl}
-					alt={data.title}
-					className={styles.containerImage}
-				/>
-			</div>
-			<div className={styles.containerDescr}>
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Accusamus adipisci numquam totam minima architecto aut
-					aspernatur eos ducimus qui neque expedita harum tempore est
-					voluptas, magni, rem error soluta nostrum.
-				</p>
-				<h4>{data.price}p</h4>
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrap}>
+                <h2 className={styles.wrapTitle}>{data.title}</h2>
+                <img
+                    src={data.imageUrl}
+                    alt={data.title}
+                    className={styles.containerImage}
+                />
+            </div>
+            <div className={styles.containerDescr}>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Accusamus adipisci numquam totam minima architecto aut
+                    aspernatur eos ducimus qui neque expedita harum tempore est
+                    voluptas, magni, rem error soluta nostrum.
+                </p>
+                <h4>{data.price}p</h4>
+            </div>
+        </div>
+    );
 };
